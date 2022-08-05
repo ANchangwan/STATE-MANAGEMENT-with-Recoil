fad45d280cc71cb1b037bda6862f3d509c0c3841
An chang wan <ssdii66@gmail.com>
2022-08-05 23:52:33 +0900

    handleSubmit and formState 추가

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input {...register("todo")} placeholder="Write a to do" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="FirstName"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="LastName"
+        />
+        <input
+          {...register("username", { required: true })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", { required: true })}
+          placeholder="Password1"
+        />
         <button>Add</button>
       </form>
     </div>
