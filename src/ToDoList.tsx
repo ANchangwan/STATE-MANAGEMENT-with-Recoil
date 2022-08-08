dec3c0a3fb48d406c15f640cdb2cb3fddeac2305
An chang wan <ssdii66@gmail.com>
2022-08-08 23:41:34 +0900

    setValue 추가

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,84 +2,23 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
-
+// setValue 값이 유효하면 input에 입력된 값을 지움
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError, //특정한 에러를 발생시켜준다.
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError("password1", {
-        message: "Password are not the same",
-        shouldFocus: true,
-      });
-    }
-    setError("extraError", { message: "Server offline" });
+  const { register, handleSubmit, watch, setValue } = useForm<IForm>();
+  const onSubmit = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
   return (
-    <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
-              message: "메일주소만 입력가능합니다!",
-            },
-          })}
-          placeholder="Email"
-        />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "firstName을 입력해주세요!",
-            validation: (value) => !value.include("changwan"),
-          })}
-          placeholder="FirstName"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "lastName을 입력해주세요!" })}
-          placeholder="LastName"
-        />
-        <span>{errors?.lasttName?.message}</span>
-        <input
-          {...register("username", { required: "userName을 입력해주세요" })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", { required: "Password을 입력해주세요" })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", { required: "password1을 입력해주세요!" })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
-        <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("toDo", { required: "Please write a To Do" })}
+        placeholder="Write here"
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
