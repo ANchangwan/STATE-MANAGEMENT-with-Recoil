ff1e51e3df98da586d1494c5f8be0e749decb6e4
An chang wan <ssdii66@gmail.com>
2022-08-11 00:26:37 +0900

    category 추가

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,15 +1,15 @@
 import { useForm } from "react-hook-form";
 import { toDoState } from "../atoms";
-import { useRecoilState } from "recoil";
+import {  useSetRecoilState } from "recoil";
 
 interface IForm {
   toDo: string;
 }
 
 function CreateToDo() {
-  const setToDos = useRecoilState(toDoState);
+  const setToDos =  useSetRecoilState(toDoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const onSubmit = ({ toDo }: IForm) => {
+  const handleValid = ({ toDo }: IForm) => {
     setToDos((oldToDos) => [
       { text: toDo, id: Date.now(), category: "TO_DO" },
       ...oldToDos,
@@ -17,7 +17,7 @@ function CreateToDo() {
     setValue("toDo", "");
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(handleValid)}>
       <input
         {...register("toDo", { required: "Please write a To Do" })}
         placeholder="Write here"
